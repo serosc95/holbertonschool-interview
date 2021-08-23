3408dbc16e83a2720d1c8f437b318ece4e8ae2eb
serosc95 <oscardleon95@gmail.com>
2021-08-23 04:21:04 +0000

    function that computes the sum of two sandpiles and README

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int stable(int grid1[3][3]);
+void print_grid1(int grid[3][3]);
+void operation_stable(int grid1[3][3]);
+
+#endif /* LISTS_H */
